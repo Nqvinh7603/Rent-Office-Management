290be74caa1e64c04b90b8469f53e0726ff0418f
Nguyễn Quang Vinh <139441823+Nqvinh7603@users.noreply.github.com>
2024-04-19 11:58:14 +0700

    save building by spring data jpa

--- src/main/java/site/rentofficevn/api/admin/BuildingAPI.java
+++ src/main/java/site/rentofficevn/api/admin/BuildingAPI.java
@@ -1,15 +1,20 @@
 package site.rentofficevn.api.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.rentofficevn.dto.BuildingDTO;
+import site.rentofficevn.service.impl.BuildingService;
 
 @RestController(value = "buildingAPIOfAmin")
 public class BuildingAPI {
+    @Autowired
+    BuildingService buildingService;
     @PostMapping("/api/building")
-    public BuildingDTO createBuilding(@RequestBody BuildingDTO newBuilding){
+    public BuildingDTO addBuilding(@RequestBody BuildingDTO newBuilding){
+        buildingService.save(newBuilding);
         return newBuilding;
     }
 }
